a6a67cee08f4e54f6293995bb3162cd529f241c4
Danielle Saldanha <danielle.saldanha98@gmail.com>
2021-10-07 19:34:12 +0200

    Added group component prototype

--- moneyapp/src/pages/GroupList/GroupList.tsx
+++ moneyapp/src/pages/GroupList/GroupList.tsx
@@ -1,6 +1,18 @@
-import { IonContent, IonHeader, IonPage } from "@ionic/react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonCard,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonIcon,
+} from "@ionic/react";
 import Toolbar from "../../components/Toolbar";
-
+import burger from "../../images/icons/burgers.svg";
+import bowling from "../../images/icons/bowling.svg";
+import { star } from "ionicons/icons";
 import "./GroupList.css";
 
 const GroupList: React.FC = () => {
@@ -10,7 +22,32 @@ const GroupList: React.FC = () => {
         <Toolbar />
       </IonHeader>
       <IonContent fullscreen>
-        Group List
+        <IonCard>
+          <IonItem color="none" lines="none">
+            <img className="icon" src={burger} />
+            <IonLabel>
+              <IonIcon icon={star}></IonIcon>
+              <IonCardTitle>Grill</IonCardTitle>
+              <IonCardSubtitle>$ 163</IonCardSubtitle>
+              <IonCardSubtitle className="updatedDate">
+                07.10.2021
+              </IonCardSubtitle>
+            </IonLabel>
+          </IonItem>
+        </IonCard>
+        <IonCard>
+          <IonItem color="none" lines="none">
+            <img className="icon" src={bowling} />
+            <IonLabel>
+              <IonIcon icon={star}></IonIcon>
+              <IonCardTitle>Impreza</IonCardTitle>
+              <IonCardSubtitle>$ 50</IonCardSubtitle>
+              <IonCardSubtitle className="updatedDate">
+                20.09.2021
+              </IonCardSubtitle>
+            </IonLabel>
+          </IonItem>
+        </IonCard>
       </IonContent>
     </IonPage>
   );

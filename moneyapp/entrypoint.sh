083fcd4b77bb5f5184661ef275138398fdbf6555
SpeeritX <Jedrek.185@gmail.com>
2021-11-21 18:55:55 +0100

    Fix bug with not refreshing group details after changing group

--- moneyapp/entrypoint.sh
+++ moneyapp/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# if ! [[ -d "node_modules" ]]
-# then
+if ! [[ -d "node_modules" ]]
+then
     echo "Installing node modules..."
     npm install
     echo "Installing ionic cli..."
     npm i -g @ionic/cli
-# fi
+fi
 
 echo "Serving Ionic..."
 ionic serve
--- moneyapp/src/pages/GroupDetails/GroupDetails.tsx
+++ moneyapp/src/pages/GroupDetails/GroupDetails.tsx
@@ -15,7 +15,7 @@ import {
   IonToast,
   useIonViewWillEnter,
 } from "@ionic/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { RouteComponentProps } from "react-router";
 import Toolbar from "../../components/Toolbar";
@@ -33,7 +33,7 @@ import { faPen, faShareAlt } from "@fortawesome/free-solid-svg-icons";
 const GroupDetails: React.FC<RouteComponentProps> = ({ history }) => {
   const { groupId } = useParams<{ groupId: string }>();
   const [isGroupLoaded, setIsGroupLoaded] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [groupDetails, setGroup] = useState<Group | null>(null);
   const [present] = useIonAlert();
   const [showCopiedToast, setCopiedShowToast] = useState(false);
@@ -49,12 +49,14 @@ const GroupDetails: React.FC<RouteComponentProps> = ({ history }) => {
   const navigateToEditGroup = () => {
     history.push(`/groups/${groupId}/edit`);
   };
-  
-  const fethGroupsDetails = async () => {
+
+  const fethGroupsDetails = useCallback(async () => {
     const group = await ApiClient.instance.getGroup(parseInt(groupId));
     setGroup(group);
+    setIsLoading(false);
     setIsGroupLoaded(true);
-  };
+  }, [groupId])
+  
 
   const copyToClipboard = async (code: string) => {
     await Clipboard.write({
@@ -70,7 +72,7 @@ const GroupDetails: React.FC<RouteComponentProps> = ({ history }) => {
     setIsLoading(false);
 
     present({
-      cssClass:"share-code-alert",
+      cssClass: "share-code-alert",
       header: "Share the code with friends!",
       message: code,
       buttons: [
@@ -82,108 +84,106 @@ const GroupDetails: React.FC<RouteComponentProps> = ({ history }) => {
 
   useEffect(() => {
     fethGroupsDetails();
-  }, []);
-  
+  }, [groupId, fethGroupsDetails]);
+
   useIonViewWillEnter(() => {
     fethGroupsDetails();
   });
 
-  if (!isGroupLoaded) {
-    return (
+  return (
+    <IonPage>
+      <IonHeader>
+        <Toolbar history={history} />
+      </IonHeader>
       <IonContent>
-        <IonLoading isOpen={true} message={"Loading..."} />
-      </IonContent>
-    );
-  } else {
-    return (
-      <IonPage>
-        <IonHeader>
-          <Toolbar history={history} />
-        </IonHeader>
-        <IonContent>
-          <IonLoading isOpen={isLoading} message={"Loading..."} />
-          <IonToast
-            isOpen={showCopiedToast}
-            onDidDismiss={() => setCopiedShowToast(false)}
-            message="Code copied to clipboard"
-            position="top"
-            color="primary"
-            mode="ios"
-            duration={1000}
-          />
-          <IonList className="container">
-            <IonGrid className="group-info">
-              <div className="manage-group-icons">
-                <IconButton size="big" onClick={presentCodeAlert}>
-                  <FontAwesomeIcon
-                    size="2x"
-                    icon={faShareAlt}
-                  ></FontAwesomeIcon>
-                </IconButton>
-                <IconButton size="big" onClick={navigateToEditGroup}>
-                  <FontAwesomeIcon size="2x" icon={faPen}></FontAwesomeIcon>
-                </IconButton>
-              </div>
-              <h2>{groupDetails!.name}</h2>
-              <div className="group-info-container">
-                <div>
-                  <IonImg
-                    className="icon group-image"
-                    src={Icons.instance.icon(groupDetails!.icon)}
-                  />
+        <IonLoading isOpen={isLoading} message={"Loading..."} />
+        <IonToast
+          isOpen={showCopiedToast}
+          onDidDismiss={() => setCopiedShowToast(false)}
+          message="Code copied to clipboard"
+          position="top"
+          color="primary"
+          mode="ios"
+          duration={1000}
+        />
+        <IonList className="container">
+          <IonGrid className="group-info">
+            {isGroupLoaded && (
+              <>
+                <div className="manage-group-icons">
+                  <IconButton size="big" onClick={presentCodeAlert}>
+                    <FontAwesomeIcon
+                      size="2x"
+                      icon={faShareAlt}
+                    ></FontAwesomeIcon>
+                  </IconButton>
+                  <IconButton size="big" onClick={navigateToEditGroup}>
+                    <FontAwesomeIcon size="2x" icon={faPen}></FontAwesomeIcon>
+                  </IconButton>
                 </div>
-                <div className="group-info-column">
-                  <IonRow className="group-info-row">
-                    <div>
-                      <IonLabel>Total cost:</IonLabel>
-                    </div>
-                    <div>
-                      <IonLabel>${groupDetails!.totalCost}</IonLabel>
-                    </div>
-                  </IonRow>
-                  <IonRow className="group-info-row">
-                    <div>
-                      <IonLabel>Balance:</IonLabel>
-                    </div>
-                    <div>
-                      <IonLabel
-                        color={
-                          groupDetails!.userBalance >= 0 ? "success" : "danger"
-                        }
-                      >
-                        ${groupDetails!.userBalance.toFixed(2)}
-                      </IonLabel>
-                    </div>
-                  </IonRow>
-                </div>
-              </div>
-            </IonGrid>
-
-            <IonRow>
-              <IonButton color="primary">Settle up</IonButton>
-              <IonButton color="primary" onClick={navigateToAddExpense}>
-                New expense
-              </IonButton>
-            </IonRow>
-            <IonCard className="group-members">
-              <IonList lines="none">
-                {groupDetails?.members!.map((e) => (
-                  <IonCol key={e.id}>
-                    <IonRow className="member-container">
-                      <IonLabel> {e.name}</IonLabel>
-                      <FlexSpacer className="member-spacer" flex={1} />
-                      <IonLabel> {e.balance.toFixed(2)}</IonLabel>
+                <h2>{groupDetails!.name}</h2>
+                <div className="group-info-container">
+                  <div>
+                    <IonImg
+                      className="icon group-image"
+                      src={Icons.instance.icon(groupDetails!.icon)}
+                    />
+                  </div>
+                  <div className="group-info-column">
+                    <IonRow className="group-info-row">
+                      <div>
+                        <IonLabel>Total cost:</IonLabel>
+                      </div>
+                      <div>
+                        <IonLabel>${groupDetails!.totalCost}</IonLabel>
+                      </div>
                     </IonRow>
-                  </IonCol>
-                ))}
-              </IonList>
-            </IonCard>
-            <IonButton onClick={navigateToAllExpenses}>All expenses</IonButton>
-          </IonList>
-        </IonContent>
-      </IonPage>
-    );
-  }
+                    <IonRow className="group-info-row">
+                      <div>
+                        <IonLabel>Balance:</IonLabel>
+                      </div>
+                      <div>
+                        <IonLabel
+                          color={
+                            groupDetails!.userBalance >= 0
+                              ? "success"
+                              : "danger"
+                          }
+                        >
+                          ${groupDetails!.userBalance.toFixed(2)}
+                        </IonLabel>
+                      </div>
+                    </IonRow>
+                  </div>
+                </div>
+              </>
+            )}
+          </IonGrid>
+
+          <IonRow>
+            <IonButton color="primary">Settle up</IonButton>
+            <IonButton color="primary" onClick={navigateToAddExpense}>
+              New expense
+            </IonButton>
+          </IonRow>
+          <IonCard className="group-members">
+            <IonList lines="none">
+              {groupDetails?.members!.map((e) => (
+                <IonCol key={e.id}>
+                  <IonRow className="member-container">
+                    <IonLabel> {e.name}</IonLabel>
+                    <FlexSpacer className="member-spacer" flex={1} />
+                    <IonLabel> {e.balance.toFixed(2)}</IonLabel>
+                  </IonRow>
+                </IonCol>
+              ))}
+            </IonList>
+          </IonCard>
+          <IonButton onClick={navigateToAllExpenses}>All expenses</IonButton>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default GroupDetails;
